8c9c95e7f50ee3dacb136fb73eb63f2505a249bf
sivaganka1012 <it24103953@my.sliit.lk>
2024-11-14 23:40:15 +0530

    Add files via upload

---
+++ Q1/IT24103953Lab4Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class IT24103953Lab4Q1 {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a number:");
+            int num = input.nextInt();
+            if (num==0)
+            System.out.println("The number is: Zero");
+            else if (num<0)
+            System.out.println("The number is: Negative");
+            else if (num>0)
+            System.out.println("The number is: Positive");
+        }
+}
